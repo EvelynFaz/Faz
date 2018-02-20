a1b50b89191c7c2ec319ef7e7738ef9c69687daa
JosselynEnriquez1995 <j.enriquez@superioraloasi.edu.ec>
2018-02-20 16:09:28 -0500

    Inicio del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hola mundo</h1>
+  <app-header></app-header>
+  <app-footer></app-footer>`,
 })
+
 export class AppComponent {
-  title = 'app';
+
 }
